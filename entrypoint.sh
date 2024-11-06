f8f49ca7f2b76389e52fca49718f1d6c1d895f8f
André Kühnert <andre.kuehnert@wandelbots.com>
2024-11-06 02:36:02 +0100

    fix the redirect when no trailing slash

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 envsubst '${BASE_PATH}' < /usr/share/nginx/html/index.html.template > /usr/share/nginx/html/index.html
 envsubst '${BASE_PATH}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
+envsubst '${BASE_PATH}' < /etc/nginx/conf.d/default.conf.template
 nginx -g 'daemon off;'
